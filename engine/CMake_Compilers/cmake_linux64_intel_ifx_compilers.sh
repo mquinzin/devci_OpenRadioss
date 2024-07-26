3c9a2d4a64f8b3ba46c0585a3a140b8c6f9b5815
Maxime Quinzin <98477371+mquinzin@users.noreply.github.com>
2024-07-26 14:39:39 +0200

    Delete all

--- engine/CMake_Compilers/cmake_linux64_intel_ifx_compilers.sh
+++
@@ -1,5 +0,0 @@
-# Fixed compiler switches for linux64 Intel Compilers
-Fortran_comp="ifx"
-C_comp="icx"
-CXX_comp="icx"
-CPP_comp="icpx"
--- starter/CMake_Compilers/cmake_linux64_gf_compilers.sh
+++
@@ -1,5 +0,0 @@
-# Fixed compiler switches for linux64 Gfortran binary
-Fortran_comp=gfortran
-C_comp=gcc
-CXX_comp=g++
-CPP_comp=g++
